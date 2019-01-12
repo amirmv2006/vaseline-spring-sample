de4bebbc347637c63546b5bba83846140354866a
Amir <amirmv2006@gmail.com>
2019-01-12 11:03:11 +0100

    init commit

--- books-app-spring/src/main/java/ir/amv/os/mps/tfp/BooksSpringApp.java
+++ books-app-spring/src/main/java/ir/amv/os/mps/tfp/BooksSpringApp.java
@@ -1,15 +1,7 @@
 package ir.amv.os.mps.tfp;
 
-import ir.amv.os.vaseline.basics.apis.core.server.polymorphysm.IVaselinePolymorphysmClassHolder;
-import ir.amv.os.vaseline.basics.apis.core.server.polymorphysm.defimpl.VaselinePolymorphysmClassHolderImpl;
-import ir.amv.os.vaseline.basics.spring.mapper.config.VaselineMapperConfig;
-import ir.amv.os.vaseline.basics.spring.mapper.config.custconv.BaseVaselineCustomConverterClassHolderImpl;
-import ir.amv.os.vaseline.basics.spring.mapper.config.custconv.IVaselineCustomConverterClassHolder;
-import ir.amv.os.vaseline.basics.spring.validation.config.VaselineValidationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author Amir
@@ -18,20 +10,5 @@ import org.springframework.context.annotation.Import;
 public class BooksSpringApp {
     public static void main(String[] args) {
         SpringApplication.run(BooksSpringApp.class, args);
-    }
-
-    @Bean
-    public IVaselinePolymorphysmClassHolder polymorphysmClassHolder() {
-        return new VaselinePolymorphysmClassHolderImpl();
-    }
-
-    @Bean
-    public IVaselineCustomConverterClassHolder customConverterClassHolder() {
-        return new BaseVaselineCustomConverterClassHolderImpl() {
-            @Override
-            public Class<?>[] customConverterClasses() {
-                return new Class[0];
-            }
-        };
     }
 }
